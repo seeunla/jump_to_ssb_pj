455b47e6f97a5a758ac352698ef174e606c7dae8
seeunla <ksu98712@gmail.com>
2022-08-08 12:51:48 +0900

    ResponseBody

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,11 +2,14 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @RequestMapping("/sbb")
-    public void index() {
+    @ResponseBody
+    public String index() {
         System.out.println("Hello");
+        return "bye";
     }
 }
