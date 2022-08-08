d4de34c66c1251a90d8770879d2bc4d0e1a5e16b
seeunla <ksu98712@gmail.com>
2022-08-08 14:12:26 +0900

    요청 관련 문제

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.sbb;
 
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class MainController {
+    private int i =-1;
     @RequestMapping("/sbb")
     @ResponseBody
     public String index() {
@@ -41,7 +46,58 @@ public class MainController {
     public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
         return """
                 <h1>입력된 나이 : %d</h1>
-                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                <h1>안녕하세요, Get 방식으로 오셨군요.</h1>
                 """.formatted(age);
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int plus(int a, int b) {
+        return a+b;
+    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public int minus(int a, int b) {
+        return a-b;
+    }
+
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int showIncrease() {
+        i++;
+        return i;
+    }
+
+    @GetMapping("/decrease")
+    @ResponseBody
+    public int showdecrease() {
+        i--;
+        return i;
+    }
+
+    @GetMapping("/mbti")
+    @ResponseBody
+    public String mbti(String name) {
+        name = "홍길동";
+        String mbti = "infp";
+        return mbti;
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String gugudan(Integer dan, Integer limit) {
+        if(limit == null ) {
+            limit = 9;
+        }
+
+        if(dan == null) {
+            dan = 9;
+        }
+        Integer finalDan = dan;
+        return IntStream.rangeClosed(1,limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i , finalDan * i))
+                .collect(Collectors.joining("<br>\n"));
     }
 }
