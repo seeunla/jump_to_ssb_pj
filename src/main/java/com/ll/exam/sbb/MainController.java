1e27d8011ed5fed4d16cdca8f2159915b75e8a87
seeunla <ksu98712@gmail.com>
2022-08-08 12:09:55 +0900

    22

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,7 +2,6 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
