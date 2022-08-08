e3b91ec8f325b22b33cf4dc936ad7b2ddb06dd0e
seeunla <ksu98712@gmail.com>
2022-08-08 12:00:11 +0900

    콘솔에 안찍힘

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,11 +2,12 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @RequestMapping("/sbb")
     public void index() {
-        System.out.println("123");
+        System.out.println("Hello");
     }
 }
