213614ceea31d6e4a3d8eee73753860d3abe7177
seeunla <ksu98712@gmail.com>
2022-08-11 10:47:15 +0900

    답변 등록까지

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,23 +1,39 @@
 package com.ll.exam.sbb.question;
 
+import com.ll.exam.sbb.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
-    @RequestMapping("question/list")
+    @RequestMapping("/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
 
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+
+    @RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
+
 }
