65a801d162d583796001f33d7ba64aab1ab540a3
seeunla <107592967+seeunla@users.noreply.github.com>
2022-08-18 13:44:13 +0900

    페이지 함수 테스트

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -104,6 +107,15 @@ public class QuestionRepositoryTests {
     }
 
     @Test
+    void findAllpageable() {
+        // Pageble : 한 페이지에 몇개의 아이템이 나와야 하는지 + 현재 몇 페이지인지)
+        Pageable pageable = PageRequest.of(0, lastSampleDataId);
+        Page<Question> page = questionRepository.findAll(pageable);
+
+        assertThat(page.getTotalPages()).isEqualTo(1);
+   }
+
+    @Test
     void findBySubject() {
         Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
         assertThat(q.getId()).isEqualTo(1);
@@ -130,10 +142,10 @@ public class QuestionRepositoryTests {
 
         if (run ==false) return;
 
-        IntStream.rangeClosed(3,30).forEach(id -> {
+        IntStream.rangeClosed(3,300).forEach(id -> {
             Question q = new Question();
             q.setSubject("%d번 질문".formatted(id));
-            q.setContent("%번 질문의 내용".formatted(id));
+            q.setContent("%d번 질문의 내용".formatted(id));
             q.setCreateDate(LocalDateTime.now());
             questionRepository.save(q);
         });
