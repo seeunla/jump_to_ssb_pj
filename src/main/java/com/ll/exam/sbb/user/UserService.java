541276ea1f11cf63a05da179d784d0ec75c854ce
seeunla <107592967+seeunla@users.noreply.github.com>
2022-08-19 10:56:39 +0900

    회원가입시 중복관련 에러가 발생 했을 때, 정확히 어떠한 정보 때문에 문제가 되는건지 체크하여 오류 메시지에 명확히 기술

--- src/main/java/com/ll/exam/sbb/user/UserService.java
+++ src/main/java/com/ll/exam/sbb/user/UserService.java
@@ -1,7 +1,7 @@
 package com.ll.exam.sbb.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +13,22 @@ public class UserService {
     // 스프링이 책임지고 PasswordEncoder 타입의 객체를 만들어야 하는 상황
     private final PasswordEncoder passwordEncoder;
 
-    public SiteUser create(String username, String email, String password) {
+    public SiteUser create(String username, String email, String password) throws SignupUsernameDuplicatedException, SignupEmailDuplicatedException{
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
-        this.userRepository.save(user);
+
+        try {
+            userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            if (userRepository.existsByUsername(username)) {
+                throw new SignupUsernameDuplicatedException("이미 사용중인 username 입니다.");
+            } else {
+                throw new SignupEmailDuplicatedException("이미 사용중인 email 입니다.");
+            }
+        }
+
         return user;
     }
 
