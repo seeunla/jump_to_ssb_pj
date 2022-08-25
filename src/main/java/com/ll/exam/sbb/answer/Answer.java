5d7553082c9d7920314e74da26f9717d1a151e51
seeunla <107592967+seeunla@users.noreply.github.com>
2022-08-25 14:08:15 +0900

    Answer, Question 에 modifyDate 칼럼 추가

--- src/main/java/com/ll/exam/sbb/answer/Answer.java
+++ src/main/java/com/ll/exam/sbb/answer/Answer.java
@@ -30,4 +30,6 @@ public class Answer {
 
     @ManyToOne
     private SiteUser author;
+
+    private LocalDateTime modifyDate;
 }
--- src/main/java/com/ll/exam/sbb/question/Question.java
+++ src/main/java/com/ll/exam/sbb/question/Question.java
@@ -44,4 +44,6 @@ public class Question {
         getAnswerList().add(answer);
     }
 
+    private LocalDateTime modifyDate;
+
 }
