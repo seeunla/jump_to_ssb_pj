581ab178f63e7433feffcf6bccc9d7d846694a72
seeunla <107592967+seeunla@users.noreply.github.com>
2022-08-25 15:25:24 +0900

    질문 수정 화면

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,10 +1,12 @@
 package com.ll.exam.sbb.question;
 
 import com.ll.exam.sbb.answer.AnswerForm;
+import com.ll.exam.sbb.exception.DataNotFoundException;
 import com.ll.exam.sbb.user.SiteUser;
 import com.ll.exam.sbb.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -69,6 +72,18 @@ public class QuestionController {
     @GetMapping("/modify/{id}")
     public String questionModify(Principal principal, QuestionForm questionForm, @PathVariable("id") Integer id) {
         Question question = questionService.getQuestion(id);
-        if
+
+        if (question == null ) {
+            throw new DataNotFoundException("%d번 질문은 존재하지 않습니다.");
+        }
+
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+
+        return "question_form";
     }
 }
